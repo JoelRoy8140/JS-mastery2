133302ef74a9ca1fd44b4bb85113a62ba16d2104
Joel Roy <joelroy8140@gmail.com>
2024-03-29 23:40:01 +0530

    done

---
+++ variables.js
@@ -0,0 +1,6 @@
+const accountId = 123456;
+let accountEmail = "joelroy1234@gmail.com";
+var accountPassword = "password1234";
+let accountName = "Joel Roy";
+let accountCity = "Bangalore";
+console.table({accountId, accountEmail, accountPassword, accountName, accountCity});
