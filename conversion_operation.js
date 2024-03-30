6059b8ea3d80426a29e2973459934eddde093b0f
Joel Roy <joelroy8140@gmail.com>
2024-03-30 10:11:44 +0530

    ..

--- conversion_operation.js
+++ conversion_operation.js
@@ -1,9 +1,9 @@
-let score = "zvfdv"
+// let score = "zvfdv"
 
 //console.log(typeof score);
 //console.log(typeof(score)); 
 
-let valueInNumber = Number(score);
+// let valueInNumber = Number(score);
 //console.log(typeof valueInNumber);
 //console.log(valueInNumber);
 
@@ -23,9 +23,44 @@ let valueInNumber = Number(score);
 // empty => false
 // Joel => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
 
 // Number to string => 33 => "33"
+
+
+// ****************************************Operations*************************************//
+
+
+let value = 3
+let negValue = -value
+console.log(-value)
+
+// console.log(value + 3)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = " world"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log(2 + "2")
+console.log("2" + 2)
+console.log("4" + 2 + 2)
+
+console.log(+true);
+
+let num1, num2, num3
+
+num1 = num2 = num3 =  2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
