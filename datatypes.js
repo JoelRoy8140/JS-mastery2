5f478fc887c996b0967e799cac8c5e7332558c03
Joel Roy <joelroy8140@gmail.com>
2024-03-30 09:30:56 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version
+
+// alert("3+3") // we are using node js not browser
+
+//  console.log(3+3); console.log("Joel") // dont do this , cause code readablity should be high
+
+let name = "Joel"
+let age = 21
+let isLoggedIn = false
+let state;
+
+//primitive data types
+
+
+//number => 2 to power 53
+//BigInt => 2 to power 53 and beyond
+//string => "Joel" or 'Joel'
+//boolean => true or false
+// null => standalone value
+// undefined => i have given the value but not declared
+// symbol => unique value
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
+
