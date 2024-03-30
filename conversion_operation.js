7e1c7cac418252ee1e8563fac7e93b1a227382ec
Joel Roy <joelroy8140@gmail.com>
2024-03-30 09:53:19 +0530

    conversion_operation.js

---
+++ conversion_operation.js
@@ -0,0 +1,31 @@
+let score = "zvfdv"
+
+//console.log(typeof score);
+//console.log(typeof(score)); 
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// 33abc => NaN
+// True => 1
+// False => 0
+
+
+// let isLoggedIn = "joel" 
+
+// let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);
+
+// 1 => true
+// 0 => false
+// empty => false
+// Joel => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
+// Number to string => 33 => "33"
