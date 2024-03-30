8e7685966b8a9d8256a15e40c9e6b77a7998d73d
Joel Roy <joelroy8140@gmail.com>
2024-03-30 10:21:47 +0530

    comparison.js

---
+++ comparison.js
@@ -0,0 +1,20 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);    // comparison convert null to a number , treating it as 0 that's why (3) null>=0 is true and (1) null>0 is false
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0); // comparison convert undefined to a number , treating it as NaN that's why (3) undefined>=0 is false and (1) undefined>0 is false
+
+
+console.log("2" === 2);
