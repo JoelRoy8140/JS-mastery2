3ae19f81b07a0aef766bafd94e431274965efead
Joel Roy <joelroy8140@gmail.com>
2024-04-04 21:51:03 +0530

    Scopes

---
+++ 03_Bascis/Scopes.js
@@ -0,0 +1,16 @@
+
+if (true){
+    let a = 40
+    const b = 50
+    var c = 60
+    
+}
+
+for (let i = 0; i < 10; i++){
+    console.log(i)
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);   // intresting thing is that var is not block scoped but let and const are block scoped
+
