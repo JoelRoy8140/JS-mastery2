a4cf19fd2dc23941754793acdfecf8a049a1e2bd
Joel Roy <joelroy8140@gmail.com>
2024-04-05 20:54:20 +0530

    Scopes

--- 03_Bascis/Scopes.js
+++ 03_Bascis/Scopes.js
@@ -7,10 +7,51 @@ if (true){
 }
 
 for (let i = 0; i < 10; i++){
-    console.log(i)
+    // console.log(i)
 }
 
 // console.log(a);
 // console.log(b);
-console.log(c);   // intresting thing is that var is not block scoped but let and const are block scoped
+// console.log(c);   // intresting thing is that var is not block scoped but let and const are block scoped
+
+function one(){
+    const username = "Joel"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+if(true){
+    const username = "Joel"
+    if(username === "Joel") {
+
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+ // ****************************************intresting****************************
+
+console.log( addone(5));
+
+ function addone(num){
+    return num + 1
+
+ }
+
+addTwo(5)
+const addTwo  = function(num){
+    return num + 2
+ }
+
 
